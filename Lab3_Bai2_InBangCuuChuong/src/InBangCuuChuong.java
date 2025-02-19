7a5d171c2af4d050ef15cec147106555d410eec4
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-02-19 09:46:44 +0700

    Bảng cửu chương

--- Lab3_Bai2_InBangCuuChuong/src/InBangCuuChuong.java
+++ Lab3_Bai2_InBangCuuChuong/src/InBangCuuChuong.java
@@ -7,7 +7,7 @@ public class InBangCuuChuong {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Chuong trinh in Bảng cửu chương");
 		for (int i = 1; i <= 9; i++) {
-			System.out.printf("Cửu chương %d\n", i);
+			System.out.printf("Cửu chương %d: \n", i);
 			for(int j = 1; j <= 10; j++)
 			{
 				System.out.printf("%d * %d = %d \n", i, j, i*j);
